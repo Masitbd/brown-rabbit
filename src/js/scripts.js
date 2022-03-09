fddeaebf8a84bb211c70ddf922e6c7d0753d4a49
Murad Sarker <sarker24@gmail.com>
2022-03-09 01:25:05 +0100

    Add detail page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,3 @@
-/* window.onload = function () {
-  var el = document.createElement("div");
-  el.innerHTML = "RABBIT TEST ";
-  var div = document.getElementById("headline");
-  insertAfter(div, el); */
-
-/* FUNCTIONS */
-
-// Inserts newNode after referenceNode
-/*   function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  }
-}; */
+const detail = () => {
+  window.location.href = "/src/detail-information.html";
+};
