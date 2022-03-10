b44c2a1d889806159b2d65c01f36bf23b54776d3
Murad Sarker <sarker24@gmail.com>
2022-03-10 03:00:05 +0100

    Add paginatin

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,3 +34,52 @@ loadAllImages();
 const detail = () => {
   window.location.href = "/src/detail-information.html";
 };
+
+function doSearch() {
+  const text = document.getElementById("searchBtn");
+  /*  if (window.find(text) && e.keyCode === 13) {
+    console.log(window.find(text));
+  } */
+  /*  const textValue = text.value;
+   */
+  text.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      // console.log(textValue);
+      const textValue = text.value;
+
+      event.preventDefault();
+      window.find(textValue);
+      console.log(window.find(textValue));
+    }
+  });
+}
+
+function showPagination() {
+  var nombrePage = $(".content").length;
+
+  showPage = function (pagination) {
+    if (pagination < 0 || pagination >= nombrePage) return;
+
+    $(".content").hide().eq(pagination).show();
+    $("#pagin li").removeClass("active").eq(pagination).addClass("active");
+  };
+
+  // Go to Left
+  $(".prev").click(function () {
+    showPage($("#pagin ul .active").index() - 1);
+  });
+
+  // Go to Right
+  $(".next").click(function () {
+    showPage($("#pagin ul .active").index() + 1);
+  });
+
+  $("#pagin ul a").click(function (e) {
+    e.preventDefault();
+    showPage($(this).parent().index());
+  });
+
+  showPage(0);
+}
+
+showPagination();
